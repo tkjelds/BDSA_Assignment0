3a2e9fd3cb578324593b425c5a9f41979b3b86dd
tkjelds <tkjelds385@gmail.com>
2022-09-04 14:54:52 +0200

    added expanded solution for error handling and tests

--- MyApp.Tests/UnitTest1.cs
+++ MyApp.Tests/UnitTest1.cs
@@ -45,5 +45,21 @@ public class UnitTest1
         result.Should().Be(true);
 
     }
+    [Fact]
+    public void testStringToIntConverter_Fail(){
+        var STICon = new StringToIntConverter();
+
+        var result = STICon.Convert("test");
+
+        result.Should().Be(-1);
+    }
+    [Fact]
+    public void testStringToIntConverter_Succees(){
+        var STICon = new StringToIntConverter();
+
+        var result = STICon.Convert("100");
+
+        result.Should().Be(100);
+    }
 
 }
--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -4,14 +4,24 @@ namespace MyApp{
     internal class program{
         static void Main(string[] args){
             var LeapYearFunc = new LeapYear();
+            var StringConverter = new StringToIntConverter();
             var LeapYearString = Console.ReadLine();
-            int LeapYear = Int32.Parse(LeapYearString);
+            int LeapYear = StringConverter.Convert(LeapYearString);
+            if(LeapYear == -1) {Console.WriteLine("Cannot parse input"); return;}
             if(LeapYearFunc.IsLeapYear(LeapYear)){
                 Console.WriteLine("yay");
             } else
             {
                 Console.WriteLine("nay");
             }
+        }
+    }
+    public class StringToIntConverter{
+        public int Convert(string intString){
+            int result;
+            bool isConverted = Int32.TryParse(intString,out result);
+            if(!isConverted) return -1;
+            return result;
         }
     }
     public class LeapYear{
